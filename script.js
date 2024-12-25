5c20bf16e80d32830d75ea060bff67c5a1590b89
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-25 20:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function isFibonacci(num) {
-		// Write your code here
+function isPerfectSquare(x) {
+    return x >= 0 && Math.sqrt(x) % 1 === 0;
 }
+
+function isFibonacci(num) {
+    if (num < 0) return false;
+    // Check if 5*n^2 + 4 or 5*n^2 - 4 is a perfect square
+    return isPerfectSquare(5 * num * num + 4) || isPerfectSquare(5 * num * num - 4);
+}
+
 const input = parseInt(prompt("Enter a number"));
-alert(isFibonacci(input));
+alert(isFibonacci(input)); 
